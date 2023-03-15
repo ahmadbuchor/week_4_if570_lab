2e0067b3571b0d6f3379240d8d95178e80cb2968
ahmadbuchor <ahmad.buchori@student.umn.ac.id>
2023-03-15 22:30:44 +0700

    Displaying the right image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -27,18 +27,35 @@ class MainActivity : AppCompatActivity() {
      */
 
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
+
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the TextView with the dice roll
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
+
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
-}
 
 class Dice(val numSides:Int) {
     fun roll(): Int {
-        return (1..numSides).random()
+        return (1 ..numSides).random()
     }
 }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+        // Do a dice roll when the app starts
+        rollDice()
+    }
